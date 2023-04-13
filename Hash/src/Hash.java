77597e9e9c0ab47c9238a9fdeca5015afe9fed7f
LopezJimenezCarlos <lopezcarlosuem@gmail.com>
2023-04-13 22:22:31 +0200

    Mohammed va a ser el Redactor

--- Hash/src/Hash.java
+++ Hash/src/Hash.java
@@ -2,45 +2,57 @@ public class Hash<Valor> {
 
 
     Celda<Valor>[] contenedor;
-    int capacidad;
-    float alfaMaximo;
+    int numElementos;
+    float alfaMaximo ;
 
 
-    public static int n = 7;
+
 
     public Hash() {
-        this.contenedor = new Celda[7];
-        this.capacidad = 0;
-        this.alfaMaximo = 0.75f;
+        contenedor = new Celda[7];
+        for(int i = 0; i<7; i++){
+            contenedor[i] = new Celda<Valor>();
+        }
+        this.numElementos = 7;
+        this.alfaMaximo = 0.8f;
+
     }
 
     public Hash(int capacidad) {
-        this.capacidad = capacidad;
+        numElementos = capacidad;
+        this.contenedor = new Celda[capacidad];
+        for(int i = 0; i<capacidad; i++){
+            contenedor[i] = new Celda<Valor>();
+        }
 
     }
 
     public Hash(int capacidad, float alfaMaximo) {
 
-        this.capacidad = capacidad;
+        numElementos = capacidad;
         this.alfaMaximo = alfaMaximo;
+        this.contenedor = new Celda[capacidad];
+        for(int i = 0; i<capacidad; i++){
+            contenedor[i] = new Celda<Valor>();
+        }
+
     }
 
     //FUNCIONES PÚBLICAS
     public void insertar(int clave, Valor v) {
         int colision = 0;
         int indice = funcionHash(clave, colision);
-        if (contenedor[indice] == null) {
-            contenedor[indice] = new Celda<>(clave, v);
-            contenedor[indice].setEstado(1);
-        } else {
-            while (contenedor[indice] != null) {
-                colision++;
-                indice = funcionHash(clave, colision);
-            }
-            contenedor[indice] = new Celda<>(clave, v);
-            contenedor[indice].setEstado(1);
+        if (factorCarga() >= alfaMaximo) {
+            redimensionar();
         }
+        while (contenedor[indice].getEstado() == 1) {
+            colision++;
+            indice = funcionHash(clave, colision);
+        }
+        contenedor[indice] = new Celda<>(clave, v);
+        contenedor[indice].setEstado(1);
     }
+
 
 
     public  boolean borrar(int clave) {
@@ -53,18 +65,19 @@ public class Hash<Valor> {
         }return borrado;
     }
 
-    public Valor get(int clave){
-        Valor value = null;
-        for (int i=0; i<contenedor.length; i++){
-            if (contenedor[i].getClave() == clave){
-                value =contenedor[i].getValor();
-                break;
-            }else {
-                return (Valor) Integer.valueOf(1);
+    public Valor get(int clave) {
+        int colision = 0;
+        int indice = funcionHash(clave, colision);
+        while (contenedor[indice] != null && colision < contenedor.length) {
+            if (contenedor[indice].getEstado() == 1 && contenedor[indice].getClave() == clave) {
+                return contenedor[indice].getValor();
             }
+            colision++;
+            indice = funcionHash(clave, colision);
         }
-        return value ;
+        return null;
     }
+
     public  boolean esVacia() {
         boolean vacia = true;
         for (int i = 0; i < contenedor.length; i++) {
@@ -74,16 +87,15 @@ public class Hash<Valor> {
 
         }return vacia;
     }
-    public static float getAlfa(){
-        return 0;
+    public  float getAlfa(){
+        return alfaMaximo;
     }
 
-    public static void setAlfa(float puta){
+    public  void setAlfa(float nuevoAlfa){
+        alfaMaximo = nuevoAlfa;
+    }
 
-    }
-    public static float getAlfa(int clave){
-        return 0;
-    }
+
     public  int getNumElementos(){
         int contador = 0;
         for (int i=0; i<contenedor.length; i++){
@@ -92,8 +104,9 @@ public class Hash<Valor> {
             }
         }return contador;
     }
-    public static float factorCarga(){
-        return 0;
+    public  float factorCarga(){
+       float nfloat=getNumElementos();
+        return nfloat/numElementos;
     }
 
 
@@ -109,13 +122,13 @@ public class Hash<Valor> {
         }return colosion;
 
     }
-    private static int funcionHash(int clave, int colision){
+    private  int funcionHash(int clave, int colision){
         int h1 = hash1(clave);
         int h2 = hash2(clave, colision);
-        return h1+h2;
+        return (h1+h2)%contenedor.length;
     }
-    private static int hash1(int clave){
-        int h1 = clave % n;
+    private  int hash1(int clave){
+        int h1 = clave % contenedor.length;
         return h1;
     }
     private static int hash2(int clave, int colision){
@@ -123,8 +136,29 @@ public class Hash<Valor> {
         return h2;
     }
 
-    public static void redimensionar(){
-
+    public  void redimensionar(){
+        int nuevoTamano = siguientePrimo(getNumElementos());
+        Celda[] nuevoContenedor = new Celda[nuevoTamano];
+        for (int i=0; i<contenedor.length; i++){
+            if (contenedor[i].getEstado() == 1){
+                int colision = 0;
+                int indice = funcionHash(contenedor[i].getClave(), colision);
+                if (nuevoContenedor[indice] == null) {
+                    nuevoContenedor[indice] = new Celda<>(contenedor[i].getClave(), contenedor[i].getValor());
+                    nuevoContenedor[indice].setEstado(1);
+                } else {
+                    while (nuevoContenedor[indice] != null) {
+                        colision++;
+                        indice = funcionHash(contenedor[i].getClave(), colision);
+                    }
+                    nuevoContenedor[indice] = new Celda<>(contenedor[i].getClave(), contenedor[i].getValor());
+                    nuevoContenedor[indice].setEstado(1);
+                }
+            }
+        }
+        contenedor = nuevoContenedor;
+        int nuevoNumElementos = getNumElementos();
+        nuevoNumElementos = nuevoTamano;
     }
     public static boolean esPrimo(int numero) {
         if (numero <= 1) {
--- Hash/src/Pruebas.java
+++ Hash/src/Pruebas.java
@@ -4,16 +4,22 @@ public class Pruebas {
 
 
     public static void main(String[] args) {
-        // Crear una tabla hash
-        Hash<Integer> hash = new Hash<>(4, 0.75f);
+      //Ejemplo de uso de la clase Hash
 
-        Celda<Integer> celda = new Celda<>(1, 69);
-        // Agregar un valor a la tabla hash
-        hash.insertar(1, 69);
+        Hash<String> hash = new Hash<>(7, 0.8f);
+        hash.insertar(1, "Hola");
+        hash.insertar(2, "Mundo");
+        hash.insertar(3, "Cruel");
+        hash.insertar(4, "!");
+        hash.insertar(5, "!");
+        hash.insertar(6, "!");
+       hash.borrar(3);
 
-        // Obtener un valor de la tabla hash
-        int valor = hash.get(0);
-        System.out.println(valor);  // Imprime "Hola"
+
+        System.out.println(hash.get(1));
+        System.out.println(hash.get(2));
+        System.out.println(hash.get(3));
+        System.out.println(hash.get(4));
     }
 }
 
